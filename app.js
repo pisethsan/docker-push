ccb98f2ebaa3278e7b63f662a4d9b7ee9b5bdfc5
Piseth San <pisethsan67167@gmail.com>
2025-10-08 15:50:42 +0700

    docker 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Docker!');
+    res.send('Hello, Docker 2');
 });
 
 app.listen(PORT, () => {
